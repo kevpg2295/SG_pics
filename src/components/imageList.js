ddaad0404d43effdc57d795dcb8f593e1d3f3590
Kevin Parada <kevin.paradag22@gmail.com>
2022-02-08 18:02:14 -0500

    Added image card comp

--- src/components/imageList.js
+++ src/components/imageList.js
@@ -1,11 +1,15 @@
 import react from "react";
+import "./imageList.css"
+import ImageCard from "./ImageCard"
+
+
 
 const ImageList = ({ images }) => {
   const imgList = images.map(({description, urls, id }) => {
     return <img alt={description} src={urls.regular} key={id} />;
   });
 
-  return <div> {imgList}</div>;
+  return <div className="image-list"> {imgList}</div>;
 };
 
 export default ImageList;
